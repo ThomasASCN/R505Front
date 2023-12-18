8c3fe755141a81b232483457c27d63d30030642f
ThomasASCN <thomas.de.la.asuncion.pro@gmail.com>
2023-12-18 10:25:43 +0100

    initialisation services et guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+import { ProfilComponent } from './pages/profil/profil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path :'', component: AccueilComponent},
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
