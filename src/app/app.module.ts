a3bfd3df1e63e7b7e69b17ef2fb1f2dda80ad48d
ThomasASCN <thomas.de.la.asuncion.pro@gmail.com>
2023-12-18 10:06:24 +0100

    feat page profil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './pages/accueil/accueil.component';
+import { ProfilComponent } from './pages/profil/profil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccueilComponent
+    AccueilComponent,
+    ProfilComponent
   ],
   imports: [
     BrowserModule,
