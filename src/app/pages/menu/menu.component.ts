12cc97fb1ec8d52b07e927635b314c1a3656adb2
ThomasASCN <thomas.de.la.asuncion.pro@gmail.com>
2024-01-11 22:09:00 +0100

    feat logout

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,17 +1,27 @@
+// Dans votre composant menu.component.ts
 import { Component } from '@angular/core';
+import { ApiService } from '../../shared/services/api.service';
 import { Router } from '@angular/router';
-@Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
-})
-export class MenuComponent {
-  constructor(private router: Router) {}
 
-  navigateTo(path: string) {
-    this.router.navigate([path]);
+@Component({
+  selector: 'app-menu', 
+  templateUrl: './menu.component.html', 
+  styleUrls: ['./menu.component.scss'] 
+})
+
+export class MenuComponent {
+
+
+
+  constructor(private apiService: ApiService, private router: Router) {}
+  navigateTo(route: string) {
+    this.router.navigateByUrl(route);
+  }
+  onLogout() {
+    this.apiService.logout().then(() => {
+      this.router.navigate(['/login']); 
+    }).catch((error: any) => {
+      console.error('Erreur lors de la déconnexion', error);
+    });
   }
 }
-
-
-
